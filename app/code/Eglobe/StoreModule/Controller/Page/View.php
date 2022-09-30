b111af69a3dc938bfe22ffcf28fece51070d23a6
aparnaeglobe <aparna@eglobeits.com>
2022-09-30 10:23:04 +0530

    Magento training-tasks-30-sep-2022

---
+++ app/code/Eglobe/StoreModule/Controller/Page/View.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eglobe\StoreModule\Controller\Page;
+
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class View extends Action
+{
+    public function execute() {
+		
+		echo "Hello World!";
+       
+    }
+}
